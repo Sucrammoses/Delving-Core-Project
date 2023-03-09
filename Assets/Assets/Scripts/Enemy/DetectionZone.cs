a1441ac3aca5981c583528605383493df5144f20
Sucrammoses <56644890+Sucrammoses@users.noreply.github.com>
2023-03-09 17:24:03 -0500

    Goblin Enemy Changes
    Made a few modifications to the goblin enemy and their script. Goblin Enemy can now move towards the player and has some basic animations set up. (1st Push didn't work)

--- Assets/Assets/Scripts/Enemy/DetectionZone.cs
+++ Assets/Assets/Scripts/Enemy/DetectionZone.cs
@@ -4,19 +4,50 @@ using UnityEngine;
 
 public class DetectionZone : MonoBehaviour
 {
-    Collider2D collider;
-    public List<Collider2D> detectedColliders = new List<Collider2D>();
+    public bool EnemyIsMoving=false;
 
-    private void Awake() 
+    public float speed = 1f;
+    private Transform EnemyTarget;
+    private Vector2 direct;
+    private Animator Animation;
+
+    private void Update()
     {
-        collider = GetComponent<Collider2D>();
+        if (EnemyTarget != null)
+        {
+            EnemyIsMoving = true;
+            float EnemyMovement = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, EnemyTarget.position, EnemyMovement);
+        }
+        else
+        {
+            EnemyIsMoving = false;
+        }
+
+        if (EnemyIsMoving == false)
+        {
+            Animation.SetFloat("MoveX", 1);
+        }
+        else
+        {
+
+        }
     }
-    private void OnTriggerEnter2D(Collider2D collision) 
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        detectedColliders.Add(collision);
+        if (other.gameObject.tag == "Player")
+        {
+            EnemyTarget = other.transform;
+        }
     }
-    private void OnTriggerExit2D(Collider2D collision) 
+
+    private void OnTriggerExit2D(Collider2D other)
     {
-        detectedColliders.Remove(collision);
+        if (other.gameObject.tag == "Player")
+        {
+            EnemyTarget = null;
+        }
     }
-}
+
+
+    }
