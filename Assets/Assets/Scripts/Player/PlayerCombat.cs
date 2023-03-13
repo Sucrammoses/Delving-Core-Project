cf3599ec6a903b7dbe173b07b5bf63d71d70d572
Sucrammoses <56644890+Sucrammoses@users.noreply.github.com>
2023-03-13 16:29:49 -0400

    Player Character Attack
    The player now has an attack animation that activates when the left mouse button is clicked. The animation that plays corelates with the directiont he player is faced.

--- Assets/Assets/Scripts/Player/PlayerCombat.cs
+++ Assets/Assets/Scripts/Player/PlayerCombat.cs
@@ -1,15 +1,33 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 public class PlayerCombat : MonoBehaviour
 {
     // Update is called once per frame
     public Animator animator;
+    public float RateofAttack = 2.0f;
+    private float NextAttack = 0f;
+    private Vector2 input;
     void Update()
     {
-        if (Input.GetButton("Fire1"))
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+        var TargetPos = transform.position;
+        
         {
-            Attack();
+            if (Input.GetButton("Fire1"))
+            {
+                TargetPos.x += input.x;
+                TargetPos.y += input.y;
+
+                animator.SetFloat("MoveX", input.x);
+                animator.SetFloat("MoveY", input.y);
+                if (Time.time >= NextAttack)
+                {
+                    Attack();
+                    NextAttack = Time.time + 1f / RateofAttack;
+                }
+            }
         }
         void Attack()
         {
